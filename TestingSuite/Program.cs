8f562e9264541fc9f63fcca20e4885f791c0b11f
Onion <madeggs@googlemail.com>
2010-05-08 22:47:34 +0100

    Adding TestingSuite testing code.

---
+++ TestingSuite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingSuite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            spacecraft.PlayerIDPacket P = new spacecraft.PlayerIDPacket();
+            P.Username = new byte[1024];
+            P.Username[0] = 0xEE;
+            P.Username[1023] = 0xFF;
+
+            P.Key = new byte[1024];
+            P.Key[0] = 0xAA;
+            P.Key[1023] = 0xBB;
+
+            P.Version = 0x07;
+
+            string s = BitConverter.ToString(P);
+        }
+    }
+}
