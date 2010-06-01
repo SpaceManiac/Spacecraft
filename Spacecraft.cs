3bd55ebf2fad5957b4126c9b1a2cbb1c0a5646fc
Magic Number <Ewan@LANNING.(none)>
2010-06-01 21:14:42 +0100

    uuh...

--- Spacecraft.cs
+++ Spacecraft.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
+//using System.Threading;
 using System.Web;
 
 namespace spacecraft
