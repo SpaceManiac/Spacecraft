84fdee0a599d9d2a67d3a86543f058df6f88a593
SpaceManiac <tad.hardesty@suddenlink.net>
2010-06-03 16:42:49 -0500

    Cleanup part 2: TestingSuite converted to tabs, files moved

--- TestingSuite/Program.cs
+++ TestingSuite/Program.cs
@@ -5,20 +5,20 @@ using spacecraft;
 
 namespace spacecraft
 {
-    class Program
-    {
-        static void Main()
-        {
-            PositionUpdatePacket p = new PositionUpdatePacket();
-            p.Heading = 197;
-            p.Pitch = 117;
-            p.PlayerID = 65;
-            p.X = (short) 19;
-            p.Y = (short) 85;
-            p.Z = (short) 147;
-            byte[] b = p.ToByteArray();
-            Console.WriteLine(b);
+	class Program
+	{
+		static void Main()
+		{
+			PositionUpdatePacket p = new PositionUpdatePacket();
+			p.Heading = 197;
+			p.Pitch = 117;
+			p.PlayerID = 65;
+			p.X = (short) 19;
+			p.Y = (short) 85;
+			p.Z = (short) 147;
+			byte[] b = p.ToByteArray();
+			Console.WriteLine(b);
 
-        }
-    }
+		}
+	}
 }
