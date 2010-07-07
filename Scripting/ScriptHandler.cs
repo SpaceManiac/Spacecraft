0bc059bd7517833e62fed5bdee5f1fca1ab909ac
Magic Number <Ewan@LANNING.(none)>
2010-07-07 21:18:06 +0100

    More commands.

--- Scripting/ScriptHandler.cs
+++ Scripting/ScriptHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using TclWrap;
+using System.Text;
 
 namespace spacecraft
 {
@@ -23,6 +24,9 @@ namespace spacecraft
             Interpreter.CreateCommand("SetTile", new TclAPI.TclCommand(ScriptSetTile));
             Interpreter.CreateCommand("GetTile", new TclAPI.TclCommand(ScriptGetTile));
             Interpreter.CreateCommand("Broadcast", new TclAPI.TclCommand(ScriptBroadcast));
+            Interpreter.CreateCommand("tell", new TclAPI.TclCommand(ScriptSendMessage));
+            Interpreter.CreateCommand("players", new TclAPI.TclCommand(ScriptGetPlayers));
+            Interpreter.CreateCommand("getplayer", new TclAPI.TclCommand(ScriptGetPlayerStats));
 
             Initialized = true;
         }
@@ -125,5 +129,63 @@ namespace spacecraft
 			TclAPI.SetResult(interp, "");
 			return TclAPI.TCL_OK;
 		}
+
+        static int ScriptSendMessage(IntPtr clientData, IntPtr interp, int argc, IntPtr argsPtr)
+        {
+            string[] args = TclAPI.GetArgumentArray(argc, argsPtr);
+
+            if (argc != 3)
+            {
+                TclAPI.SetResult(interp, "Wrong number of arguments, expected 2, got " + argc.ToString());
+                return TclAPI.TCL_ERROR;
+            }
+
+            Player P = Server.theServ.GetPlayer(args[1]);
+            P.PrintMessage(args[2]);
+
+            TclAPI.SetResult(interp, "");
+            return TclAPI.TCL_OK;
+        }
+
+        static int ScriptGetPlayers(IntPtr clientData, IntPtr interp, int argc, IntPtr argsPtr)
+        {
+            string[] args = TclAPI.GetArgumentArray(argc, argsPtr);
+
+            if (argc != 1)
+            {
+                TclAPI.SetResult(interp, "Wrong number of arguments, expected 0, got " + argc.ToString());
+                return TclAPI.TCL_ERROR;
+            }
+
+            StringBuilder Result = new StringBuilder();
+
+            foreach (Player P in Server.theServ.Players)
+            {
+                if (P != null)
+                {
+                    Result.Append(P.name);
+                    Result.Append(" ");
+                }
+            }
+
+            TclAPI.SetResult(interp, Result.ToString());
+            return TclAPI.TCL_OK;
+
+        }
+
+        static int ScriptGetPlayerStats(IntPtr clientData, IntPtr interp, int argc, IntPtr argsPtr)
+        {
+            string[] args = TclAPI.GetArgumentArray(argc, argsPtr);
+
+            if (argc != 2)
+            {
+                TclAPI.SetResult(interp, "Wrong number of arguments, expected 1, got " + argc.ToString());
+                return TclAPI.TCL_ERROR;
+            }
+
+            Player Target = Server.theServ.GetPlayer(args[1]);
+
+        }
+
 	}
 }
