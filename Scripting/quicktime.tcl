bdccf85f326d76d2d8a17ea7bddddd6de2ea3969
SpaceManiac <tad.hardesty@suddenlink.net>
2010-07-10 15:36:41 -0500

    Minor change to quicktime.tcl script (new /countdown command)

--- Scripting/quicktime.tcl
+++ Scripting/quicktime.tcl
@@ -1,5 +1,9 @@
-for {set i 0} {$i < 10} {incr i} {
-	set n [expr {10 - $i}]
-	after [expr {$i * 1000}] "broadcast $n..."
+proc quicktime {args} {
+	for {set i 0} {$i < 10} {incr i} {
+		set n [expr {10 - $i}]
+		after [expr {$i * 1000}] "broadcast $n..."
+	}
+	after 10000 "broadcast GOOOOOOOOO!"
 }
-after 10000 "broadcast GOOOOOOOOO!"
+
+#createChatCommand "countdown" Mod "start a 10-second countdown" quicktime
