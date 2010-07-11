8c30f9f7ef7a809a973f0a9698164641218912cb
SpaceManiac <tad.hardesty@suddenlink.net>
2010-07-11 14:52:46 -0500

    Ran cleanup

--- Constants/Blocks.cs
+++ Constants/Blocks.cs
@@ -66,7 +66,7 @@ namespace spacecraft
 		Fire = 51,
 		InfiniteWater = 52,*/
 
-        Maximum = Obsidian
+		Maximum = Obsidian
 	}
 
 	public static class BlockInfo
--- Map/McLevelLoading.cs
+++ Map/McLevelLoading.cs
@@ -8,27 +8,27 @@ using System.Net;
 
 namespace spacecraft
 {
-    public class McLevelLoading
-    {
-        public static Map Load(string filename)
-        {
+	public class McLevelLoading
+	{
+		public static Map Load(string filename)
+		{
 			
-            StreamReader RawReader = new StreamReader(filename);
-            GZipStream Reader = new GZipStream(RawReader.BaseStream, CompressionMode.Decompress);
+			StreamReader RawReader = new StreamReader(filename);
+			GZipStream Reader = new GZipStream(RawReader.BaseStream, CompressionMode.Decompress);
 
-            NBT.BinaryTag tag = NBT.NbtParser.ParseTagStream(Reader);
-            
-            Reader.Close();
-            RawReader.Close();
-            
-            // TODO: parse BinaryTag into Map.
+			NBT.BinaryTag tag = NBT.NbtParser.ParseTagStream(Reader);
+			
+			Reader.Close();
+			RawReader.Close();
+			
+			// TODO: parse BinaryTag into Map.
 
-            return new Map();
-        }
+			return new Map();
+		}
 
-        public static void Save(Map M, string filename)
-        {
+		public static void Save(Map M, string filename)
+		{
 			// TODO: maybe saving. Issue with gzip formats?
-        }
-    }
+		}
+	}
 }
